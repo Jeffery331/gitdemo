7f2f0c15b2f5849547e44c97d959ce303a9b4207
Jeffery331 <zjy226331@163.com>
2020-04-03 19:36:14 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -5,3 +5,17 @@ Created on Sat Jun  8 12:50:21 2019
 @author: Jeffery
 """
 
+
+import matplotlib.pyplot as plt
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+#文本状态查看
+with open('停用词表.txt','r') as g:        #导入停用词表
+    stop_word_list = g.read()
+stop_word_list = stop_word_list.split()        #获得停用词表列表
+
+punctuation_list = [',','.','!','?','\'',';',':','\"', '“','‘','’','”']        #未用正则
